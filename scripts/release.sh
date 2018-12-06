61942b012ef04051ca2a0e661c526fcf2b2b51f2
David Lattimore <dml@google.com>
2018-12-06 11:25:25 +1100

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -e
-cargo build
-cargo test --all
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 {version}" >&2
+  exit 1
+fi
 if ! git diff-index --quiet HEAD --; then
   echo "Please commit all changes first" >&2
   exit 1
 fi
+VERSION="$1"
+git pull --rebase
+perl -pi -e 's/^version = "[\d\.]+"/version = "'$VERSION'"/;\
+    s/^evcxr = \{ version = "=[\d\.]+"/evcxr = \{ version = "='$VERSION'"/' \
+  evcxr/Cargo.toml \
+  evcxr_repl/Cargo.toml \
+  evcxr_jupyter/Cargo.toml
+cargo build
+cargo test --all
+git commit -a -m "Bump vesion to $VERSION"
 cd evcxr
 cargo publish
 cd ../evcxr_repl
