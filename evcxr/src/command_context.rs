8fe38fe8e0c76ffd14cedb51a45af25a0072ccc4
David Bernard <david.bernard.31@gmail.com>
2018-12-26 23:14:37 +0100

    fullfill timing info for command (like dep)
    - rollback add of external crate if fail to add
    - keep the error for failing ‘:dep’

--- evcxr/src/command_context.rs
+++ evcxr/src/command_context.rs
@@ -35,15 +35,15 @@ impl CommandContext {
     }
 
     pub fn execute(&mut self, to_run: &str) -> Result<EvalOutputs, Error> {
-        if to_run.is_empty() {
-            return Ok(EvalOutputs::new());
-        }
-        if to_run.starts_with(':') {
-            return self.process_command(to_run);
-        }
         use std::time::Instant;
         let start = Instant::now();
-        let result = self.eval_context.eval(to_run);
+        let result = if to_run.is_empty() {
+            Ok(EvalOutputs::new())
+        } else if to_run.starts_with(':') {
+            self.process_command(to_run)
+        } else {
+            self.eval_context.eval(to_run)
+        };
         let duration = start.elapsed();
         match result {
             Ok(mut m) => {
@@ -68,7 +68,7 @@ impl CommandContext {
         if line == ":internal_debug" {
             let debug_mode = !self.eval_context.debug_mode();
             self.eval_context.set_debug_mode(debug_mode);
-            return text_output(format!("Internals debugging: {}", debug_mode));
+            text_output(format!("Internals debugging: {}", debug_mode))
         } else if line == ":vars" {
             let mut outputs = EvalOutputs::new();
             outputs
@@ -77,19 +77,16 @@ impl CommandContext {
             outputs
                 .content_by_mime_type
                 .insert("text/html".to_owned(), self.vars_as_html());
-            return Ok(outputs);
+            Ok(outputs)
         } else if line == ":clear" {
-            self.eval_context.clear()?;
+            self.eval_context.clear().map(|_| EvalOutputs::new())
         } else if let Some(captures) = ADD_DEP_RE.captures(line) {
-            if let Err(error) = self
+            self
                 .eval_context
                 .add_extern_crate(captures[1].to_owned(), captures[2].to_owned())
-            {
-                bail!("{}", error);
-            }
         } else if line == ":last_compile_dir" {
             if let Some(dir) = &self.eval_context.last_compile_dir() {
-                return text_output(format!("{:?}", dir));
+                text_output(format!("{:?}", dir))
             } else {
                 bail!("Nothing has been compiled yet");
             }
@@ -100,10 +97,10 @@ impl CommandContext {
                 "2"
             };
             self.eval_context.set_opt_level(new_level)?;
-            return text_output(format!("Optimization: {}", self.eval_context.opt_level()));
+            text_output(format!("Optimization: {}", self.eval_context.opt_level()))
         } else if line == ":timing" {
             self.print_timings = !self.print_timings;
-            return text_output(format!("Timing: {}", self.print_timings));
+            text_output(format!("Timing: {}", self.print_timings))
         } else if line == ":explain" {
             if self.last_errors.is_empty() {
                 bail!("No last error to explain");
@@ -116,7 +113,7 @@ impl CommandContext {
                         bail!("Sorry, last error has no explanation");
                     }
                 }
-                return text_output(all_explanations);
+                text_output(all_explanations)
             }
         } else if line == ":last_error_json" {
             let mut errors_out = String::new();
@@ -127,7 +124,7 @@ impl CommandContext {
             }
             bail!(errors_out);
         } else if line == ":help" {
-            return text_output(
+            text_output(
                 ":vars             List bound variables and their types\n\
                  :opt              Toggle optimization\n\
                  :explain          Print explanation of last error\n\
@@ -138,11 +135,10 @@ impl CommandContext {
                  :timing           Toggle printing of how long evaluations take\n\
                  :last_error_json  Print the last compilation error as JSON (for debugging)\n\
                  :internal_debug   Toggle various internal debugging code",
-            );
+            )
         } else {
             bail!("Unrecognised command {}", line);
         }
-        Ok(EvalOutputs::new())
     }
 
     fn vars_as_text(&self) -> String {
