36e116056aaba560cde2b006855b0fbfc0d94286
David Lattimore <dml@google.com>
2019-08-25 08:52:34 +1000

    Refactor the way commands are processed

--- evcxr/src/command_context.rs
+++ evcxr/src/command_context.rs
@@ -35,12 +35,16 @@ impl CommandContext {
     }
 
     pub fn execute(&mut self, to_run: &str) -> Result<EvalOutputs, Error> {
+        use regex::Regex;
         use std::time::Instant;
+        lazy_static! {
+            static ref COMMAND_RE: Regex = Regex::new("(:[^ ]+)( +(.*))?").unwrap();
+        }
         let start = Instant::now();
         let result = if to_run.is_empty() {
             Ok(EvalOutputs::new())
-        } else if to_run.starts_with(':') {
-            self.process_command(to_run)
+        } else if let Some(captures) = COMMAND_RE.captures(to_run) {
+            self.process_command(&captures[1], captures.get(3).map(|m| m.as_str()))
         } else {
             self.eval_context.eval(to_run)
         };
@@ -75,89 +79,98 @@ impl CommandContext {
         Ok(outputs)
     }
 
-    fn process_command(&mut self, line: &str) -> Result<EvalOutputs, Error> {
-        use regex::Regex;
-        lazy_static! {
-            static ref ADD_DEP_RE: Regex = Regex::new(":dep ([^= ]+) *= *(.+)").unwrap();
-        }
-        lazy_static! {
-            static ref OPT_RE: Regex = Regex::new(":opt( (0|1|2))?").unwrap();
-        }
-        lazy_static! {
-            static ref PRESERVE_VARS_RE: Regex =
-                Regex::new(":preserve_vars_on_panic (0|1)").unwrap();
-        }
-        if line == ":internal_debug" {
-            let debug_mode = !self.eval_context.debug_mode();
-            self.eval_context.set_debug_mode(debug_mode);
-            text_output(format!("Internals debugging: {}", debug_mode))
-        } else if line == ":load_config" {
-            self.load_config()
-        } else if line == ":version" {
-            text_output(env!("CARGO_PKG_VERSION"))
-        } else if line == ":vars" {
-            let mut outputs = EvalOutputs::new();
-            outputs
-                .content_by_mime_type
-                .insert("text/plain".to_owned(), self.vars_as_text());
-            outputs
-                .content_by_mime_type
-                .insert("text/html".to_owned(), self.vars_as_html());
-            Ok(outputs)
-        } else if let Some(captures) = PRESERVE_VARS_RE.captures(line) {
-            self.eval_context.preserve_vars_on_panic = &captures[1] == "1";
-            text_output(format!(
-                "Preserve vars on panic: {}",
-                self.eval_context.preserve_vars_on_panic
-            ))
-        } else if line == ":clear" {
-            self.eval_context.clear().map(|_| EvalOutputs::new())
-        } else if let Some(captures) = ADD_DEP_RE.captures(line) {
-            self.eval_context
-                .add_extern_crate(captures[1].to_owned(), captures[2].to_owned())
-        } else if line == ":last_compile_dir" {
-            text_output(format!("{:?}", self.eval_context.last_compile_dir()))
-        } else if let Some(captures) = OPT_RE.captures(line) {
-            let new_level = if let Some(n) = captures.get(2) {
-                n.as_str()
-            } else if self.eval_context.opt_level() == "2" {
-                "0"
-            } else {
-                "2"
-            };
-            self.eval_context.set_opt_level(new_level)?;
-            text_output(format!("Optimization: {}", self.eval_context.opt_level()))
-        } else if line == ":timing" {
-            self.print_timings = !self.print_timings;
-            text_output(format!("Timing: {}", self.print_timings))
-        } else if line == ":time_passes" {
-            self.eval_context
-                .set_time_passes(!self.eval_context.time_passes());
-            text_output(format!("Time passes: {}", self.eval_context.time_passes()))
-        } else if line == ":explain" {
-            if self.last_errors.is_empty() {
-                bail!("No last error to explain");
-            } else {
-                let mut all_explanations = String::new();
-                for error in &self.last_errors {
-                    if let Some(explanation) = error.explanation() {
-                        all_explanations.push_str(explanation);
-                    } else {
-                        bail!("Sorry, last error has no explanation");
-                    }
+    fn process_command(&mut self, command: &str, args: Option<&str>) -> Result<EvalOutputs, Error> {
+        match command {
+            ":internal_debug" => {
+                let debug_mode = !self.eval_context.debug_mode();
+                self.eval_context.set_debug_mode(debug_mode);
+                text_output(format!("Internals debugging: {}", debug_mode))
+            }
+            ":load_config" => self.load_config(),
+            ":version" => text_output(env!("CARGO_PKG_VERSION")),
+            ":vars" => {
+                let mut outputs = EvalOutputs::new();
+                outputs
+                    .content_by_mime_type
+                    .insert("text/plain".to_owned(), self.vars_as_text());
+                outputs
+                    .content_by_mime_type
+                    .insert("text/html".to_owned(), self.vars_as_html());
+                Ok(outputs)
+            }
+            ":preserve_vars_on_panic" => {
+                self.eval_context.preserve_vars_on_panic = args == Some("1");
+                text_output(format!(
+                    "Preserve vars on panic: {}",
+                    self.eval_context.preserve_vars_on_panic
+                ))
+            }
+            ":clear" => self.eval_context.clear().map(|_| EvalOutputs::new()),
+            ":dep" => {
+                use regex::Regex;
+                let args = if let Some(v) = args {
+                    v
+                } else {
+                    bail!(":dep requires arguments")
+                };
+                lazy_static! {
+                    static ref DEP_RE: Regex = Regex::new("([^= ]+) *= *(.+)").unwrap();
                 }
-                text_output(all_explanations)
+                if let Some(captures) = DEP_RE.captures(args) {
+                    self.eval_context
+                        .add_extern_crate(captures[1].to_owned(), captures[2].to_owned())
+                } else {
+                    bail!("Invalid :dep command. Expected: name = ...");
+                }
             }
-        } else if line == ":last_error_json" {
-            let mut errors_out = String::new();
-            for error in &self.last_errors {
-                use std::fmt::Write;
-                write!(&mut errors_out, "{}", error.json)?;
-                errors_out.push('\n');
+            ":last_compile_dir" => {
+                text_output(format!("{:?}", self.eval_context.last_compile_dir()))
             }
-            bail!(errors_out);
-        } else if line == ":help" {
-            text_output(
+            ":opt" => {
+                let new_level = if let Some(n) = args {
+                    &n
+                } else if self.eval_context.opt_level() == "2" {
+                    "0"
+                } else {
+                    "2"
+                };
+                self.eval_context.set_opt_level(new_level)?;
+                text_output(format!("Optimization: {}", self.eval_context.opt_level()))
+            }
+            ":timing" => {
+                self.print_timings = !self.print_timings;
+                text_output(format!("Timing: {}", self.print_timings))
+            }
+            ":time_passes" => {
+                self.eval_context
+                    .set_time_passes(!self.eval_context.time_passes());
+                text_output(format!("Time passes: {}", self.eval_context.time_passes()))
+            }
+            ":explain" => {
+                if self.last_errors.is_empty() {
+                    bail!("No last error to explain");
+                } else {
+                    let mut all_explanations = String::new();
+                    for error in &self.last_errors {
+                        if let Some(explanation) = error.explanation() {
+                            all_explanations.push_str(explanation);
+                        } else {
+                            bail!("Sorry, last error has no explanation");
+                        }
+                    }
+                    text_output(all_explanations)
+                }
+            }
+            ":last_error_json" => {
+                let mut errors_out = String::new();
+                for error in &self.last_errors {
+                    use std::fmt::Write;
+                    write!(&mut errors_out, "{}", error.json)?;
+                    errors_out.push('\n');
+                }
+                bail!(errors_out);
+            }
+            ":help" => text_output(
                 ":vars             List bound variables and their types\n\
                  :opt [level]      Toggle/set optimization level\n\
                  :explain          Print explanation of last error\n\
@@ -171,9 +184,8 @@ impl CommandContext {
                  :last_error_json  Print the last compilation error as JSON (for debugging)\n\
                  :time_passes      Toggle printing of rustc pass times (requires nightly)\n\
                  :internal_debug   Toggle various internal debugging code",
-            )
-        } else {
-            bail!("Unrecognised command {}", line);
+            ),
+            _ => bail!("Unrecognised command {}", command),
         }
     }
 
