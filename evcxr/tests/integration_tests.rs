937a225abd535e23a9a7f64657305ab56133a0d5
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-07-29 23:10:52 -0600

    Added support for line comments between :deps (#234)
    * Added support for line comments between :deps

--- evcxr/tests/integration_tests.rs
+++ evcxr/tests/integration_tests.rs
@@ -991,10 +991,11 @@ let s2 = "さび  äää"; let s2: String = 42; fn foo() -> i32 {
             "\
             :dep this_crate_does_not_exist = \"12.34\"\n\
             :dep foo = { path = \"/this/path/does/not/exist\" }\n\
+            // This is a comment interleaved with commands\n\
             :an_invalid_command
             "
         )),
-        vec!["error 1:6-1:41", "error 2:6-2:50", "error 3:1-3:20"]
+        vec!["error 1:6-1:41", "error 2:6-2:50", "error 4:1-4:20"]
     );
 
     // Make sure missing close parenthesis are reported as expected. Note, we still don't check the
