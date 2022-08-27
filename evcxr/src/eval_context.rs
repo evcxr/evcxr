990e748569b2e5ae7b88990f5df1c447ca8a1a40
David Lattimore <dvdlttmr@gmail.com>
2022-08-28 09:12:33 +1000

    Update version of tokio used for async code

--- evcxr/src/eval_context.rs
+++ evcxr/src/eval_context.rs
@@ -835,7 +835,7 @@ impl EvalContext {
                     if error.code() == Some("E0728") && !state.async_mode {
                         state.async_mode = true;
                         if !state.external_deps.contains_key("tokio") {
-                            state.add_dep("tokio", "\"0.2\"")?;
+                            state.add_dep("tokio", "\"1.20.1\"")?;
                             // Rewrite Cargo.toml, since the dependency will probably have been
                             // validated in the process of being added, which will have overwritten
                             // Cargo.toml
