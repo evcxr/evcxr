cfcc7e68f23c44091d23953cd5af70452fbd5818
David Lattimore <dvdlttmr@gmail.com>
2023-10-23 10:27:48 +1100

    Split release scripts so that actual releasing is a separate process

---
+++ scripts/pre-release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+VERSION=$(perl -e \
+  'while (<>) { if (/^# Version (\d+\.\d+\.\d+) \(unreleased\)/) {print "$1"}}' \
+  RELEASE_NOTES.md \
+)
+if [ -z "$VERSION" ]; then
+  echo "RELEASE_NOTES.md doesn't contain an unreleased version" >&2
+  exit 1
+fi
+if ! git diff-index --quiet HEAD --; then
+  echo "Please commit all changes first" >&2
+  exit 1
+fi
+MIN_RUST_VER=$(grep ^rust-version evcxr/Cargo.toml | cut -d'"' -f2)
+if [ -z "$MIN_RUST_VER" ]; then
+  echo "Failed to determine minimum rust version" >&2
+  exit 1
+fi
+echo "Releasing $VERSION"
+git pull --rebase
+perl -pi -e 's/(^# .*) \(unreleased\)$/$1/' RELEASE_NOTES.md
+perl -pi -e 's/^version = "[\d\.]+"/version = "'$VERSION'"/;\
+    s/^evcxr = \{ version = "=[\d\.]+"/evcxr = \{ version = "='$VERSION'"/' \
+  evcxr/Cargo.toml \
+  evcxr_repl/Cargo.toml \
+  evcxr_jupyter/Cargo.toml
+cargo build
+cargo +stable test --all
+cargo +nightly test --all
+cargo +${MIN_RUST_VER}-x86_64-unknown-linux-gnu test --all
+git commit -a -m "Bump vesion to $VERSION"
+git rev-parse HEAD >.pre-release.hash
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
 set -e
-VERSION=$(perl -e \
-  'while (<>) { if (/^# Version (\d+\.\d+\.\d+) \(unreleased\)/) {print "$1"}}' \
-  RELEASE_NOTES.md \
-)
+VERSION=$(grep ^version evcxr/Cargo.toml | cut -d'"' -f2)
 if [ -z "$VERSION" ]; then
-  echo "RELEASE_NOTES.md doesn't contain an unreleased version" >&2
+  echo "Couldn't determine version" >&2
   exit 1
 fi
 if ! git diff-index --quiet HEAD --; then
   echo "Please commit all changes first" >&2
   exit 1
 fi
-MIN_RUST_VER=$(grep ^rust-version evcxr/Cargo.toml | cut -d'"' -f2)
-if [ -z "$MIN_RUST_VER" ]; then
-  echo "Failed to determine minimum rust version" >&2
+if [ $(git rev-parse HEAD) != $(cat .pre-release.hash) ]; then
+  echo "Please run ./scripts/pre-release.sh first" >&2
   exit 1
 fi
-echo "Releasing $VERSION"
-git pull --rebase
-perl -pi -e 's/(^# .*) \(unreleased\)$/$1/' RELEASE_NOTES.md
-perl -pi -e 's/^version = "[\d\.]+"/version = "'$VERSION'"/;\
-    s/^evcxr = \{ version = "=[\d\.]+"/evcxr = \{ version = "='$VERSION'"/' \
-  evcxr/Cargo.toml \
-  evcxr_repl/Cargo.toml \
-  evcxr_jupyter/Cargo.toml
-cargo build
-cargo +stable test --all
-cargo +nightly test --all
-cargo +${MIN_RUST_VER}-x86_64-unknown-linux-gnu test --all
-git commit -a -m "Bump vesion to $VERSION"
+
 cd evcxr
 cargo publish
 # Wait a but before we try to push packages that depend on the version we just
