a241a8abf80dc1fb4165c85a9ed7a1524dd55a14
David Lattimore <dvdlttmr@gmail.com>
2025-03-20 12:31:21 +1100

    Back out "jupyter kernel: Restore sending of iopub idle message on errors and commands"
    This backs out commit b77a4a4a73c57ea8f79834b6d11619ec00d6dfcf.

--- evcxr/src/eval_context.rs
+++ evcxr/src/eval_context.rs
@@ -910,7 +910,6 @@ impl EvalContext {
             let line = self.child_process.recv_line()?;
             if line == runtime::EVCXR_EXECUTION_COMPLETE {
                 let _ = self.stdout_sender.send(StdoutEvent::ExecutionComplete);
-                output.got_execution_complete_marker = true;
                 break;
             }
             if line == PANIC_NOTIFICATION {
@@ -1130,7 +1129,6 @@ pub struct EvalOutputs {
     pub content_by_mime_type: HashMap<String, String>,
     pub timing: Option<Duration>,
     pub phases: Vec<PhaseDetails>,
-    pub got_execution_complete_marker: bool,
 }
 
 impl EvalOutputs {
@@ -1139,7 +1137,6 @@ impl EvalOutputs {
             content_by_mime_type: HashMap::new(),
             timing: None,
             phases: Vec::new(),
-            got_execution_complete_marker: false,
         }
     }
 
